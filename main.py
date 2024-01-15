3be63bd776dee907dbd93d6d6a5b6f9da5815a28
Luigi <lugminsinlag@gmail.com>
2024-01-15 17:34:28 +0100

    style: Renaming one variable and improoving readability of the code.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def get_words_number(text: str) -> int:
     return len(text.split())
 
+
 def get_characters_count(text: str) -> dict[str,int]:
     t_list = list(text.lower())
     ch_count = {}
@@ -10,16 +11,20 @@ def get_characters_count(text: str) -> dict[str,int]:
         ch_count[t] +=1
     return ch_count
 
+
 def main():
     file_to_open = "books/frankenstein.txt"
     with open(f"{file_to_open}") as f:
         file = f.read()
+
         print(f"--- Begin report of {file_to_open} ---")
         print(f"{get_words_number(file)} words found in the document\n")
+
         ch_count = get_characters_count(file)
-        ch_list = [c for c in ch_count if c.isalpha()]
-        ch_list.sort()
-        for ch in ch_list:
+        alpha_ch_list = [c for c in ch_count if c.isalpha()]
+        alpha_ch_list.sort()
+
+        for ch in alpha_ch_list:
             print(f"The {ch} character was found {ch_count[ch]} times")
         print("--- End report ---")
 
